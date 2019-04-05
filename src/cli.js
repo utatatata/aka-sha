521da95910f6b96f37e7216494d8fb5cb90a052a
utakuma <i@utakuma.info>
2019-04-06 04:30:15 +0900

    use strict for yargs

--- src/cli.js
+++ src/cli.js
@@ -128,74 +128,77 @@ const display = async (whatAbout, word) => {
   }
 }
 
-module.exports = yargs.locale('en').command(
-  '$0 <word>',
-  'get infomation for the word',
-  yargs =>
-    yargs
-      .option('audio', {
-        desc: 'Fetches audio metadata for the word',
-        type: 'boolean',
+module.exports = yargs
+  .locale('en')
+  .strict(true)
+  .command(
+    '$0 <word>',
+    'get infomation for the word',
+    yargs =>
+      yargs
+        .option('audio', {
+          desc: 'Fetches audio metadata for the word',
+          type: 'boolean',
+        })
+        .option('definitions', {
+          alias: 'd',
+          desc: 'Returns definitions for the word',
+          type: 'boolean',
+        })
+        .option('etymologies', {
+          desc: 'Fetches etymology data',
+          type: 'boolean',
+        })
+        .option('examples', {
+          alias: 'e',
+          desc: 'Returns examples for the word',
+          type: 'boolean',
+        })
+        .option('frequency', {
+          desc: 'Returns word usage over time',
+          type: 'boolean',
+        })
+        .option('hyphenation', {
+          alias: 'h',
+          desc: 'Returns syllable information for the word',
+          type: 'boolean',
+        })
+        .option('phrases', {
+          alias: 'p',
+          desc: 'Fetches bi-gram phrases for the word',
+          type: 'boolean',
+        })
+        .option('pronunciations', {
+          alias: 'n',
+          desc: 'Returns text pronunciations for the word',
+          type: 'boolean',
+        })
+        .option('relatedWords', {
+          alias: 'r',
+          desc: 'Returns relationships from the Word Graph',
+          type: 'boolean',
+        })
+        .option('scrabbleScore', {
+          desc: 'Returns the Scrabble score for the word',
+          type: 'boolean',
+        })
+        .option('topExample', {
+          desc: 'Returns a top example for the word',
+          type: 'boolean',
+        }),
+    async argv => {
+      Object.keys(akasha.word).forEach(whatAbout => {
+        if (argv[whatAbout]) {
+          display(whatAbout, argv.word)
+        }
       })
-      .option('definitions', {
-        alias: 'd',
-        desc: 'Returns definitions for the word',
-        type: 'boolean',
-      })
-      .option('etymologies', {
-        desc: 'Fetches etymology data',
-        type: 'boolean',
-      })
-      .option('examples', {
-        alias: 'e',
-        desc: 'Returns examples for the word',
-        type: 'boolean',
-      })
-      .option('frequency', {
-        desc: 'Returns word usage over time',
-        type: 'boolean',
-      })
-      .option('hyphenation', {
-        alias: 'h',
-        desc: 'Returns syllable information for the word',
-        type: 'boolean',
-      })
-      .option('phrases', {
-        alias: 'p',
-        desc: 'Fetches bi-gram phrases for the word',
-        type: 'boolean',
-      })
-      .option('pronunciations', {
-        alias: 'n',
-        desc: 'Returns text pronunciations for the word',
-        type: 'boolean',
-      })
-      .option('relatedWords', {
-        alias: 'r',
-        desc: 'Returns relationships from the Word Graph',
-        type: 'boolean',
-      })
-      .option('scrabbleScore', {
-        desc: 'Returns the Scrabble score for the word',
-        type: 'boolean',
-      })
-      .option('topExample', {
-        desc: 'Returns a top example for the word',
-        type: 'boolean',
-      }),
-  async argv => {
-    Object.keys(akasha.word).forEach(whatAbout => {
-      if (argv[whatAbout]) {
-        display(whatAbout, argv.word)
-      }
-    })
 
-    if (
-      Object.keys(akasha.word)
-        .map(whatAbout => argv[whatAbout])
-        .every(option => option !== true)
-    ) {
-      display('definitions', argv.word)
+      if (
+        Object.keys(akasha.word)
+          .map(whatAbout => argv[whatAbout])
+          .every(option => option !== true)
+      ) {
+        display('definitions', argv.word)
+      }
     }
-  }
-)
+  )
