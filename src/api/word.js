b7fb8d96495086575de5579c901368814a58e27a
utakuma <i@utakuma.info>
2019-04-06 01:01:03 +0900

    add CLI implementation

--- src/api/word.js
+++ src/api/word.js
@@ -3,9 +3,6 @@ const base = require('./_base')
 const wordGetUri = (word, what, params = {}) =>
   base.getUri(`/word.json/${word}/${what}`, params)
 
-// const wordBase = what => (word, params = {}) =>
-//   axios.get(wordUri(word, what, params))
-
 const wordGet = what => (word, params = {}) =>
   base.get(wordGetUri(word, what, params))
 
--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,82 @@
 const yargs = require('yargs')
 const akasha = require('./aka-sha')
 
-module.exports = yargs.command(
-  '$0',
-  'default',
-  () => {},
+const definitions = data => {
+  console.log('Definitions:')
+  data.forEach(obj => {
+    console.log(`  ${obj.text}`)
+    console.log(`    (${obj.attributionText})`)
+  })
+}
+
+module.exports = yargs.locale('en').command(
+  '$0 <word>',
+  'get infomation of the word',
+  yargs =>
+    yargs
+      .option('audio', {
+        desc: 'Fetches audio metadata for the word',
+        type: 'boolean',
+      })
+      .option('definitions', {
+        alias: 'd',
+        desc: 'Returns definitions for the word',
+        type: 'boolean',
+      })
+      .option('etymologies', {
+        desc: 'Fetches etymology data',
+        type: 'boolean',
+      })
+      .option('examples', {
+        alias: 'e',
+        desc: 'Returns examples for the word',
+        type: 'boolean',
+      })
+      .option('frequency', {
+        desc: 'Returns word usage over time',
+        type: 'boolean',
+      })
+      .option('hyphenation', {
+        alias: 'h',
+        desc: 'Returns syllable information for the word',
+        type: 'boolean',
+      })
+      .option('phrases', {
+        alias: 'p',
+        desc: 'Fetches bi-gram phrases for the word',
+        type: 'boolean',
+      })
+      .option('pronunciations', {
+        alias: 'n',
+        desc: 'Returns text pronunciations for the word',
+        type: 'boolean',
+      })
+      .option('relatedWords', {
+        alias: 'r',
+        desc: 'Returns relationships from the Word Graph',
+        type: 'boolean',
+      })
+      .option('scrabbleScore', {
+        desc: 'Returns the Scrabble score for the word',
+        type: 'boolean',
+      })
+      .option('topExample', {
+        desc: 'Returns a top example for the word',
+        type: 'boolean',
+      }),
   async argv => {
-    const d = await akasha.word.definitions('hello')
-    console.log(d)
+    if (argv.definitions) {
+      const data = await akasha.word.definitions(argv.word)
+      definitions(data)
+    }
+
+    if (
+      Object.keys(akasha.word)
+        .map(apiName => argv[apiName])
+        .every(opt => opt !== true)
+    ) {
+      const data = await akasha.word.definitions(argv.word)
+      definitions(data)
+    }
   }
 )
